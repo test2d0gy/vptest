8e291f3f46a4f358daaeec95c33cb515f941c305
test2d0gy <85608764+test2d0gy@users.noreply.github.com>
2021-06-09 15:24:49 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import boto3
+
+client = boto3.client(
+    's3',
+    aws_access_key_id=AKIAIOSFODNN7EXAMPLE,
+    aws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+)
+
+## this example is taken directly from AWS documentation
